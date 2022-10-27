38b0f2102e22c125d17c53ea0b63eca629544314
aidata <aidata@10.121.3.170>
2022-10-27 10:38:16 +0300

    27.10

---
+++ Arrays/src/Nesnetabanlıprogramlama/Array.java
@@ -0,0 +1,28 @@
+package Nesnetabanlıprogramlama;
+
+public class Array {
+
+	public static void main(String[] args) {
+		
+		
+			int [] array= {32,27,64,18,95,14,90,70,60,37};
+	System.out.printf("%s%8s%n","Index","Value");
+		int temp=0;
+		
+		for(int counter=0; counter <array.length/2;counter++)
+		{
+			temp=array[counter];
+			array[counter]=array[array.length-1-counter];
+			array[array.length-1]=temp;
+		}
+		for (int counter=0; counter < array. length;counter++) {
+		
+		System.out.printf("%5d%8d%n",counter,array[counter]);
+		}
+
+	}
+	
+
+	}
+
+
