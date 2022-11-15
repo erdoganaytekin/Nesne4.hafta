843a86754472f7bfedaf0468d17687c67895a5fd
erdemaytekin <116784884+erdemaytekin@users.noreply.github.com>
2022-11-15 23:49:22 +0300

    Update Array.java

--- Arrays/src/Nesnetabanlıprogramlama/Array.java
+++ Arrays/src/Nesnetabanlıprogramlama/Array.java
@@ -5,7 +5,7 @@ public class Array {
 	public static void main(String[] args) {
 		
 		
-			int [] array= {32,27,64,18,95,14,90,70,60,37};
+			int [] array= {32,27,64,18,85,14,90,70,60,37};
 	System.out.printf("%s%8s%n","Index","Value");
 		int temp=0;
 		
@@ -18,11 +18,10 @@ public class Array {
 		for (int counter=0; counter < array. length;counter++) {
 		
 		System.out.printf("%5d%8d%n",counter,array[counter]);
-		}
+		} 
 
 	}
-	
+	  
 
 	}
-
 
